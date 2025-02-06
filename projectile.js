f6c6dbb3e2e27b67f2fa21790450a77f57a51e53
JosephOkumu <jokumu25@gmail.com>
2025-02-06 21:29:40 +0300

    feat: update position of projectile

--- projectile.js
+++ projectile.js
@@ -13,4 +13,19 @@ export class Projectile {
       this.updatePosition();
     }
   
-}
+    updatePosition() {
+        this.element.style.transform = `translate3d(${this.x - this.width/2}px, ${this.y}px, 0)`;
+      }
+    
+      update() {
+        this.y += this.velocity;
+        
+        // Deactivate projectile if it goes off screen
+        if (this.y < -this.height || this.y > 600) {
+          this.isActive = false;
+          return;
+        }
+        
+        this.updatePosition();
+      }
+    }
